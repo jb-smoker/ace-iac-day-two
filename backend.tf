baf2421ae227053910389bbda75523e7ea5a12c8
John Smoker <jsmoker@aviatrix.com>
2023-06-30 08:50:07 -0700

    commit this change

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
-    organization = "jb-smoker"
+    organization = "jb-smoker" #change testing
     workspaces {
       name = "ace-iac-day-two"
     }
